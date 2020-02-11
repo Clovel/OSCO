c988b72cbce00e8d04c21be39eaa1d3c0d482f7b
Clovis Durand <cd.clovel19@gmail.com>
2020-02-11 23:56:59 +0100

    Fixed OSCOErrorCodes.hpp
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- inc/OSCOErrorCodes.hpp
+++ inc/OSCOErrorCodes.hpp
@@ -1,26 +1,27 @@
 /**
  * @brief OSCO error codes
  * 
- * @file OSCOErrorCodes.h
+ * @file OSCOErrorCodes.hpp
  */
 
-#ifndef CAN_IP_ERROR_CODES_H
-#define CAN_IP_ERROR_CODES_H
+#ifndef OSCO_ERRORCODES_HPP
+#define OSCO_ERRORCODES_HPP
 
 /* Includes -------------------------------------------- */
 
 /* Defines --------------------------------------------- */
 
 /* Type definitions ------------------------------------ */
-    typedef enum _oscoErrorCodes {
-        OSCO_ERROR_UNKNOWN      = 0U,
-        OSCO_ERROR_NONE         = 1,
-        OSCO_ERROR_ARG          = 2,
-        OSCO_ERROR_SYS          = 3,
-        OSCO_ERROR_NET          = 4,
-        OSCO_ERROR_ALREADY_INIT = 5,
-        OSCO_ERROR_NOT_INIT     = 6,
-        OSCO_ERROR_STOPPED      = 7
-    } oscoErrorCode_t;
+typedef enum _oscoErrorCodes {
+    OSCO_ERROR_UNKNOWN      = 0U,
+    OSCO_ERROR_NONE         = 1,
+    OSCO_ERROR_ARG          = 2,
+    OSCO_ERROR_SYS          = 3,
+    OSCO_ERROR_NET          = 4,
+    OSCO_ERROR_ALREADY_INIT = 5,
+    OSCO_ERROR_NOT_INIT     = 6,
+    OSCO_ERROR_STOPPED      = 7,
+    OSCO_ERROR_DRIVER       = 8
+} oscoErrorCode_t;
 
-#endif /* CAN_IP_ERROR_CODES_H */
+#endif /* OSCO_ERRORCODES_HPP */
