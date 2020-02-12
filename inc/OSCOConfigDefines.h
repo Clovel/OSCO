e75db4488e84cab92f5e368d40979ecf51401f38
Clovis Durand <cd.clovel19@gmail.com>
2020-02-12 13:18:18 +0100

    [#4] Added CAN line relevant defines in OSCOConfigDefines.h
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- inc/OSCOConfigDefines.h
+++ inc/OSCOConfigDefines.h
@@ -13,7 +13,8 @@
 /* Includes -------------------------------------------- */
 
 /* Defines --------------------------------------------- */
-#define OSCO_MAX_CAN_DRIVERS 1U /**< MAximum dumber of OSCOCANDriver instances */
+#define OSCO_MAX_CAN_DRIVERS 1U /**< Maximum dumber of OSCOCANDriver instances & CAN lines */
+#define OSCO_CAN_LINE_1      0U /**< ID of the CAN line 1 */
 
 /* Type definitions ------------------------------------ */
 
