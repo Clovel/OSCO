e18825f0d3a46f31bacde2defa83f41caf4c0231
Clovis Durand <cd.clovel19@gmail.com>
2020-02-12 10:58:45 +0100

    Added OSCOTypes.h
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

---
+++ inc/OSCOTypes.h
@@ -0,0 +1,21 @@
+/*
+ * @brief OSCI custom public types
+ * 
+ * @file OSCOTypes.h
+ */
+
+#ifndef OSCO_TYPES_H
+#define OSCO_TYPES_H
+
+/* Includes -------------------------------------------- */
+#include "OSCOErrorCodes.h"
+
+#include <stdint.h>   /* TODO : Delete this and use custom types */
+#include <stdbool.h>  /* TODO : Delete this and use custom types */
+
+/* Defines --------------------------------------------- */
+#define NULL (void *)0U
+
+/* Type definitions ------------------------------------ */
+
+#endif /* OSCO_TYPES_H */
