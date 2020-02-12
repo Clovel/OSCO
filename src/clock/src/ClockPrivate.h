7a78dceb38a39ce13ae8fd9e0392e24eba945b4a
Clovis Durand <cd.clovel19@gmail.com>
2020-02-12 19:43:27 +0100

    [#5] Updated ClockPrivate.h Doxygen comments
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- src/clock/src/ClockPrivate.h
+++ src/clock/src/ClockPrivate.h
@@ -1,8 +1,8 @@
 /*
- * @brief Private definition of the CANDriver module's
+ * @brief Private definition of the OSCOClock module's
  * internal variables. 
  * 
- * @file CANDriverPrivate.h
+ * @file ClockPrivate.h
  */
 
 #ifndef OSCO_CLOCKPRIVATE_H
@@ -14,11 +14,13 @@
 /* Defines --------------------------------------------- */
 
 /* Type definitions ------------------------------------ */
+/**
+ * @brief OSCO Clock module internal variables
+ */
 typedef struct _canDriverPrivateVars {
-    bool initialized;
-
-    uint64_t ticks;
-    uint32_t resolution;
+    bool initialized;       /**< Flag indicating if the module is initialized */
+    uint64_t ticks;         /**< Internal clock tick counter */
+    uint32_t resolution;    /**< Internal clock tick time interval */
 } oscoCANDriverInstance_t;
 
 #endif /* OSCO_CLOCKPRIVATE_H */
