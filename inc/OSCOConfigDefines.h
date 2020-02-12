7e89091b4f27136052ff2246eee7d7f72253ece9
Clovis Durand <cd.clovel19@gmail.com>
2020-02-12 10:59:15 +0100

    Added header to define configuration settings at compile time
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

---
+++ inc/OSCOConfigDefines.h
@@ -0,0 +1,17 @@
+/*
+ * @brief Compile time configuration defines for OSCO
+ * 
+ * @file osco_can_driver.h
+ */
+
+#ifndef OSCO_CONFIGDEFINES_H
+#define OSCO_CONFIGDEFINES_H
+
+/* Includes -------------------------------------------- */
+
+/* Defines --------------------------------------------- */
+#define OSCO_MAX_CAN_DRIVERS 1U
+
+/* Type definitions ------------------------------------ */
+
+#endif /* OSCO_CONFIGDEFINES_H */
