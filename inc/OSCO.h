b46b2d47773cc24e04eb655dc5ec1fd313e0e24c
Clovis Durand <cd.clovel19@gmail.com>
2020-02-12 13:59:44 +0100

    Added OSCOClockTick to main API
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- inc/OSCO.h
+++ inc/OSCO.h
@@ -17,5 +17,19 @@
 /* Initialization functions ---------------------------- */
 
 /* Internal stack clock functions ---------------------- */
+/**
+ * @brief This function increments the OSCO stack's
+ * internal clock/counter. 
+ * 
+ * @details This function must be called at regular intervals
+ * for the stack to stays synchronized and for it to
+ * manager internal timed events like timeouts or
+ * inhibit times.
+ * Call this function several times to catch up if you are late
+ * on non-threaded embedded systems for example.
+ * 
+ * @return Error code
+ */
+oscoErrorCode_t OSCOClockTick(void);
 
 #endif /* OSCO_H */
