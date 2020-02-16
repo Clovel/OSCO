63117dc8e7b28b7c6393d81652eabbe2a7f331af
Clovis Durand <cd.clovel19@gmail.com>
2020-02-16 14:53:13 +0100

    [#6] Added TimerThread as submodule to example dependencies
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- examples/basic/main.cpp
+++ examples/basic/main.cpp
@@ -7,6 +7,8 @@
 /* Includes -------------------------------------------- */
 #include "OSCO.h"
 
+#include "TimerThread.hxx"
+
 #include <iostream>
 
 /* Defines --------------------------------------------- */
