683f24ae5f53cb92f44034acbdf1f4b6e4e5c5bd
Clovis Durand <cd.clovel19@gmail.com>
2020-02-24 12:41:16 +0100

    [#2] Started implementing Rx FIFO
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- src/rxmgr/src/RxMgr.c
+++ src/rxmgr/src/RxMgr.c
@@ -18,6 +18,7 @@
 /* Type definitions ------------------------------------ */
 
 /* Variable declarations ------------------------------- */
+oscoRxMgrInstance_t gRxMgr;
 
 /* OSCO Reception manager functions -------------------- */
 int OSCORxMgrInputMessage(const uint8_t pID, 
@@ -26,7 +27,23 @@ int OSCORxMgrInputMessage(const uint8_t pID,
     const uint8_t * const pData,
     const uint32_t pFlags)
 {
-    //
+    /* Copy the CAN Message */
+    (gRxMgr.fifo + gRxMgr.wIdx)->id    = pID;
+    (gRxMgr.fifo + gRxMgr.wIdx)->size  = pSize;
+    (gRxMgr.fifo + gRxMgr.wIdx)->flags = pFlags;
+
+    for(uint8_t i = 0U; (i < pSize) && (i < CAN_MESSAGE_MAX_SIZE); i++) {
+        (gRxMgr.fifo + gRxMgr.wIdx)->data[i] = pData[i];
+    }
+
+    /* Increment the write index */
+    gRxMgr.wIdx++;
+    if(OSCO_RX_FIFO_SIZE <= gRxMgr.wIdx) {
+        gRxMgr.wIdx = 0U;
+        gRxMgr.wIdxWrapAround++;
+    }
+
+    return 0; /* TODO : error checking */
 }
 
 oscoErrorCode_t OSCORxMgrProcess(const uint8_t pID) {
--- src/rxmgr/src/RxMgrPrivate.h
+++ src/rxmgr/src/RxMgrPrivate.h
@@ -19,11 +19,14 @@
  * @brief OSCO RxMgr module internal variables
  */
 typedef struct _rxMgrPrivateVars {
-    bool initialized;       /**< Flag indicating if the module is initialized */
+    bool initialized; /**< Flag indicating if the module is initialized */
 
-    OSCOCANMessage_t fifo[OSCO_RX_FIFO_SIZE];
-    size_t rIdx;
-    size_t wIdx;
+    /* Message-per-message FIFO related variables */
+    OSCOCANMessage_t fifo[OSCO_RX_FIFO_SIZE];   /**< Message-per-message FIFO buffer */
+    size_t rIdx;                                /**< Message-per-message FIFO buffer read index */
+    size_t wIdx;                                /**< Message-per-message FIFO buffer write index */
+    uint32_t rIdxWrapAround;                    /**< Read index wraparound counter */
+    uint32_t wIdxWrapAround;                    /**< Write index wraparound counter */
 } oscoRxMgrInstance_t;
 
 #endif /* OSCO_RXMGRPRIVATE_H */
