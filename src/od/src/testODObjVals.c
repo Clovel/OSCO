f0ceb26c60219ae2e2dabc095805001cc555a87e
Clovis Durand <cd.clovel19@gmail.com>
2020-02-27 16:23:11 +0100

    Started to build an OD from scratch
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

---
+++ src/od/src/testODObjVals.c
@@ -0,0 +1,27 @@
+/*
+ * @brief Test OD Object values
+ * 
+ * @file testODDefVals.c
+ */
+
+/* Includes -------------------------------------------- */
+/* OSCO OD module */
+#include "OSCOOD.h"
+
+/* OSCO public includes */
+#include "OSCOTypes.h"
+
+/* Object value declarations --------------------------- */
+// uint8_t objValU8[] = {
+//     //
+// };
+// uint8_t objValU16[] = {
+//     //
+// };
+uint8_t objValU32[] = {
+    0U, /* 0x1000 */
+    0U, /* 0x1001 */
+};
+// uint8_t objValU64[] = {
+//     //
+// };
