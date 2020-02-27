d0e409d92be15186752cf2e4e9c1171c44d59b56
Clovis Durand <cd.clovel19@gmail.com>
2020-02-27 20:44:30 +0100

    Added a testOD header + OD element count define
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- src/od/src/testOD.c
+++ src/od/src/testOD.c
@@ -7,6 +7,7 @@
 /* Includes -------------------------------------------- */
 /* OSCO OD module */
 #include "OSCOOD.h"
+#include "testOD.h"
 
 /* OSCO public includes */
 #include "OSCOConfigDefines.h"
@@ -41,7 +42,7 @@ extern const uint8_t minValU32[];
 extern const uint8_t minValU64[];
 
 /* CANOpen Object Dictionary declaration --------------- */
-OSCOODElement_t gOD[] = {
+OSCOODElement_t gOD[OD_OBJECT_COUNT] = {
     /* Device Type */
     {
         0x1000U,
