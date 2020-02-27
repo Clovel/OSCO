d15f21aa33a942756443a900794ae5b751f40f5c
Clovis Durand <cd.clovel19@gmail.com>
2020-02-27 21:35:54 +0100

    Added weak definitions of the RxMgr mutex functions (for macOS)
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

---
+++ src/rxmgr/src/RxMgrMutex.c
@@ -0,0 +1,23 @@
+/*
+ * @brief Weak implementation of the RxMgr module's 
+ * mutex functions.
+ * 
+ * @file RxMgrMutex.c
+ */
+
+/* Includes -------------------------------------------- */
+
+/* Defines --------------------------------------------- */
+
+/* Type definitions ------------------------------------ */
+
+/* Variable declarations ------------------------------- */
+
+/* OSCO CAN Driver functions --------------------------- */
+__attribute__((weak)) void OSCOMutexLockRxMgr(void) {
+    /* Empty */
+}
+
+__attribute__((weak)) void OSCOMutexUnlockRxMgr(void) {
+    /* Empty */
+}
