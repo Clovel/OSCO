463869fc4d370b2cf34b73714b7c32833c61e632
Clovis Durand <cd.clovel19@gmail.com>
2020-04-28 19:21:20 +0200

    Manually fixed generated C code (waiting for OSCO-OD-Gen fix)
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- examples/test-gen/node2/OSCOGenOD.c
+++ examples/test-gen/node2/OSCOGenOD.c
@@ -18,28 +18,72 @@
 /* Type definitions ------------------------------------ */
 
 /* Default value declarations -------------------------- */
+extern const uint8_t  defValBool[];
 extern const uint8_t  defValU8[];
 extern const uint16_t defValU16[];
 extern const uint32_t defValU32[];
 extern const uint64_t defValU64[];
+extern const uint8_t  defValI8[];
+extern const uint16_t defValI16[];
+extern const uint32_t defValI32[];
+extern const uint64_t defValI64[];
+extern const uint64_t defValReal32[];
+extern const uint64_t defValReal64[];
+extern const uint64_t defValVisStr[];
+extern const uint64_t defValOctStr[];
+extern const uint64_t defValUniStr[];
+extern const uint64_t defValDomain[];
 
 /* Object value declarations --------------------------- */
+extern const uint8_t  objValBool[];
 extern const uint8_t  objValU8[];
 extern const uint16_t objValU16[];
 extern const uint32_t objValU32[];
 extern const uint64_t objValU64[];
+extern const uint8_t  objValI8[];
+extern const uint16_t objValI16[];
+extern const uint32_t objValI32[];
+extern const uint64_t objValI64[];
+extern const uint64_t objValReal32[];
+extern const uint64_t objValReal64[];
+extern const uint64_t objValVisStr[];
+extern const uint64_t objValOctStr[];
+extern const uint64_t objValUniStr[];
+extern const uint64_t objValDomain[];
 
 /* Max value declarations ------------------------------ */
+extern const uint8_t  maxValBool[];
 extern const uint8_t  maxValU8[];
 extern const uint16_t maxValU16[];
 extern const uint32_t maxValU32[];
 extern const uint64_t maxValU64[];
+extern const uint8_t  maxValI8[];
+extern const uint16_t maxValI16[];
+extern const uint32_t maxValI32[];
+extern const uint64_t maxValI64[];
+extern const uint64_t maxValReal32[];
+extern const uint64_t maxValReal64[];
+extern const uint64_t maxValVisStr[];
+extern const uint64_t maxValOctStr[];
+extern const uint64_t maxValUniStr[];
+extern const uint64_t maxValDomain[];
 
 /* Min value declarations ------------------------------ */
+extern const uint8_t  minValBool[];
 extern const uint8_t  minValU8[];
 extern const uint16_t minValU16[];
 extern const uint32_t minValU32[];
 extern const uint64_t minValU64[];
+extern const uint8_t  minValI8[];
+extern const uint16_t minValI16[];
+extern const uint32_t minValI32[];
+extern const uint64_t minValI64[];
+extern const uint64_t minValReal32[];
+extern const uint64_t minValReal64[];
+extern const uint64_t minValVisStr[];
+extern const uint64_t minValOctStr[];
+extern const uint64_t minValUniStr[];
+extern const uint64_t minValDomain[];
 
 /* CANOpen Object Dictionary declaration --------------- */
 OSCOODElement_t gOD[OD_OBJECT_COUNT] = {
@@ -942,8 +986,8 @@ OSCOODElement_t gOD[OD_OBJECT_COUNT] = {
         OD_ACCESS_TYPE_CONST,
         (void *)(objValVisStr + 0),
         (void *)(defValVisStr + 0),
-        @@OBJECT_MAX_VAL_PTR@@,
-        @@OBJECT_MIN_VAL_PTR@@,
+        NULL,
+        NULL,
     },
 
 };
