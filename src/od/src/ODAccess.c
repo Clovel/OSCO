5a04ae5bb86dd3619931607a2b4bdd31f0e801e8
Clovis Durand <cd.clovel19@gmail.com>
2020-04-29 17:42:53 +0200

    Updated OD Getters
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- src/od/src/ODAccess.c
+++ src/od/src/ODAccess.c
@@ -5,12 +5,14 @@
  */
 
 /* Includes -------------------------------------------- */
+/* Generated OSCO OD */
+#include "OSCOGenOD.h"
+
 /* OSCO OD module */
 #include "OSCOOD.h"
 
 /* OSCO public includes */
 #include "OSCOTypes.h"
-#include "OSCOErrorCodes.h"
 #include "OSCOPrint.h"
 #include "OSCO.h"
 
@@ -20,15 +22,19 @@
 
 /* External variable declarations ---------------------- */
 extern OSCOODElement_t gOD[];
-extern uint16_t OSCOODObjCount;
 
 /* OD pointer getters ---------------------------------- */
 static oscoErrorCode_t getObjPtr(
     const uint16_t pIdx,
     const uint8_t pSubIdx,
-    void **pOut)
+    void ** const pOut)
 {
-    for(uint16_t i = 0U; i < OSCOODObjCount; i++) {
+    if(NULL == pOut) {
+        eprintf("[ERROR] OSCO <getObjPtr> Output ptr is NULL !\n");
+        return OSCO_ERROR_ARG;
+    }
+
+    for(uint16_t i = 0U; i < OD_OBJECT_COUNT; i++) {
         if((pIdx == gOD[i].idx) && (pSubIdx == gOD[i].subIdx)) {
             /* We found our object */
             
@@ -48,7 +54,7 @@ static oscoErrorCode_t getObjPtr(
     }
 
     eprintf("[ERROR] OSCO <getObjPtr> 0x%04X:0x%02X : Object not found !\n", pIdx, pSubIdx);
-    pOut = NULL;
+    *pOut = NULL;
 
     return OSCO_ERROR_OD_NOT_FOUND;
 }
@@ -77,19 +83,9 @@ oscoErrorCode_t OSCOODGetU8(const uint16_t pIdx,
             }
             *pOut = *(uint8_t *)lObjectPtr;
             return OSCO_ERROR_NONE;
-        case OSCO_ERROR_OD_ACCESS_DENIED:
-            eprintf("[ERROR] OSCO <OSCOODGetU8> 0x%04X:0x%02X : Access denied !\n", pIdx, pSubIdx);
-            return lErrorCode;
-        case OSCO_ERROR_OD_NOT_FOUND:
-            eprintf("[ERROR] OSCO <OSCOODGetU8> 0x%04X:0x%02X : Object not found !\n", pIdx, pSubIdx);
-            return lErrorCode;
         default:
-            /* We shouldn't be here, something went wrong */
-            eprintf("[ERROR] OSCO <OSCOODGetU8> 0x%04X:0x%02X : Unknown error from getObjPtr (%u) !\n", pIdx, pSubIdx, lErrorCode);
-            return OSCO_ERROR_UNKNOWN; /* TODO : Find better return code */
+            return lErrorCode;
     }
-
-    eprintf("[ERROR] OSCO <OSCOODGetU8> 0x%04X:0x%02X : Object not found !\n", pIdx, pSubIdx);
 
     return OSCO_ERROR_OD_NOT_FOUND;
 }
@@ -117,19 +113,9 @@ oscoErrorCode_t OSCOODGetU16(const uint16_t pIdx,
             }
             *pOut = *(uint16_t *)lObjectPtr;
             return OSCO_ERROR_NONE;
-        case OSCO_ERROR_OD_ACCESS_DENIED:
-            eprintf("[ERROR] OSCO <OSCOODGetU16> 0x%04X:0x%02X : Access denied !\n", pIdx, pSubIdx);
-            return lErrorCode;
-        case OSCO_ERROR_OD_NOT_FOUND:
-            eprintf("[ERROR] OSCO <OSCOODGetU16> 0x%04X:0x%02X : Object not found !\n", pIdx, pSubIdx);
-            return lErrorCode;
         default:
-            /* We shouldn't be here, something went wrong */
-            eprintf("[ERROR] OSCO <OSCOODGetU16> 0x%04X:0x%02X : Unknown error from getObjPtr (%u) !\n", pIdx, pSubIdx, lErrorCode);
-            return OSCO_ERROR_UNKNOWN; /* TODO : Find better return code */
+            return lErrorCode;
     }
-
-    eprintf("[ERROR] OSCO <OSCOODGetU16> 0x%04X:0x%02X : Object not found !\n", pIdx, pSubIdx);
 
     return OSCO_ERROR_OD_NOT_FOUND;
 }
@@ -157,19 +143,160 @@ oscoErrorCode_t OSCOODGetU32(const uint16_t pIdx,
             }
             *pOut = *(uint32_t *)lObjectPtr;
             return OSCO_ERROR_NONE;
-        case OSCO_ERROR_OD_ACCESS_DENIED:
-            eprintf("[ERROR] OSCO <OSCOODGetU32> 0x%04X:0x%02X : Access denied !\n", pIdx, pSubIdx);
-            return lErrorCode;
-        case OSCO_ERROR_OD_NOT_FOUND:
-            eprintf("[ERROR] OSCO <OSCOODGetU32> 0x%04X:0x%02X : Object not found !\n", pIdx, pSubIdx);
-            return lErrorCode;
         default:
-            /* We shouldn't be here, something went wrong */
-            eprintf("[ERROR] OSCO <OSCOODGetU32> 0x%04X:0x%02X : Unknown error from getObjPtr (%u) !\n", pIdx, pSubIdx, lErrorCode);
-            return OSCO_ERROR_UNKNOWN; /* TODO : Find better return code */
+            return lErrorCode;
     }
 
-    eprintf("[ERROR] OSCO <OSCOODGetU32> 0x%04X:0x%02X : Object not found !\n", pIdx, pSubIdx);
+    return OSCO_ERROR_OD_NOT_FOUND;
+}
+
+oscoErrorCode_t OSCOODGetU64(const uint16_t pIdx,
+    const uint8_t pSubIdx,
+    uint64_t * const pOut)
+{
+    if(NULL == pOut) {
+        eprintf("[ERROR] OSCO <OSCOODGetU64> Output ptr is NULL !\n");
+        return OSCO_ERROR_ARG;
+    }
+
+    void *lObjectPtr = NULL;
+
+    /* Get the pointer to the objects data */
+    oscoErrorCode_t lErrorCode = getObjPtr(pIdx, pSubIdx, &lObjectPtr);
+    switch(lErrorCode) {
+        case OSCO_ERROR_NONE:
+            /* We found the object, and we can read it */
+            if(NULL == lObjectPtr) {
+                /* We shouldn't be in this case, something went wrong */
+                eprintf("[ERROR] OSCO <OSCOODGetU64> 0x%04X:0x%02X : getObjPtr returned a NULL pointer !\n", pIdx, pSubIdx);
+                return OSCO_ERROR_OD;
+            }
+            *pOut = *(uint64_t *)lObjectPtr;
+            return OSCO_ERROR_NONE;
+        default:
+            return lErrorCode;
+    }
+
+    return OSCO_ERROR_OD_NOT_FOUND;
+}
+
+
+oscoErrorCode_t OSCOODGetI8(const uint16_t pIdx,
+    const uint8_t pSubIdx,
+    int8_t * const pOut)
+{
+    if(NULL == pOut) {
+        eprintf("[ERROR] OSCO <OSCOODGetI8> Output ptr is NULL !\n");
+        return OSCO_ERROR_ARG;
+    }
+
+    void *lObjectPtr = NULL;
+
+    /* Get the pointer to the objects data */
+    oscoErrorCode_t lErrorCode = getObjPtr(pIdx, pSubIdx, &lObjectPtr);
+    switch(lErrorCode) {
+        case OSCO_ERROR_NONE:
+            /* We found the object, and we can read it */
+            if(NULL == lObjectPtr) {
+                /* We shouldn't be in this case, something went wrong */
+                eprintf("[ERROR] OSCO <OSCOODGetI8> 0x%04X:0x%02X : getObjPtr returned a NULL pointer !\n", pIdx, pSubIdx);
+                return OSCO_ERROR_OD;
+            }
+            *pOut = *(int8_t *)lObjectPtr;
+            return OSCO_ERROR_NONE;
+        default:
+            return lErrorCode;
+    }
+
+    return OSCO_ERROR_OD_NOT_FOUND;
+}
+
+oscoErrorCode_t OSCOODGetI16(const uint16_t pIdx,
+    const uint8_t pSubIdx,
+    int16_t * const pOut)
+{
+    if(NULL == pOut) {
+        eprintf("[ERROR] OSCO <OSCOODGetI16> Output ptr is NULL !\n");
+        return OSCO_ERROR_ARG;
+    }
+
+    void *lObjectPtr = NULL;
+
+    /* Get the pointer to the objects data */
+    oscoErrorCode_t lErrorCode = getObjPtr(pIdx, pSubIdx, &lObjectPtr);
+    switch(lErrorCode) {
+        case OSCO_ERROR_NONE:
+            /* We found the object, and we can read it */
+            if(NULL == lObjectPtr) {
+                /* We shouldn't be in this case, something went wrong */
+                eprintf("[ERROR] OSCO <OSCOODGetI16> 0x%04X:0x%02X : getObjPtr returned a NULL pointer !\n", pIdx, pSubIdx);
+                return OSCO_ERROR_OD;
+            }
+            *pOut = *(int16_t *)lObjectPtr;
+            return OSCO_ERROR_NONE;
+        default:
+            return lErrorCode;
+    }
+
+    return OSCO_ERROR_OD_NOT_FOUND;
+}
+
+oscoErrorCode_t OSCOODGetI32(const uint16_t pIdx,
+    const uint8_t pSubIdx,
+    int32_t * const pOut)
+{
+    if(NULL == pOut) {
+        eprintf("[ERROR] OSCO <OSCOODGetI32> Output ptr is NULL !\n");
+        return OSCO_ERROR_ARG;
+    }
+
+    void *lObjectPtr = NULL;
+
+    /* Get the pointer to the objects data */
+    oscoErrorCode_t lErrorCode = getObjPtr(pIdx, pSubIdx, &lObjectPtr);
+    switch(lErrorCode) {
+        case OSCO_ERROR_NONE:
+            /* We found the object, and we can read it */
+            if(NULL == lObjectPtr) {
+                /* We shouldn't be in this case, something went wrong */
+                eprintf("[ERROR] OSCO <OSCOODGetI32> 0x%04X:0x%02X : getObjPtr returned a NULL pointer !\n", pIdx, pSubIdx);
+                return OSCO_ERROR_OD;
+            }
+            *pOut = *(int32_t *)lObjectPtr;
+            return OSCO_ERROR_NONE;
+        default:
+            return lErrorCode;
+    }
+
+    return OSCO_ERROR_OD_NOT_FOUND;
+}
+
+oscoErrorCode_t OSCOODGetI64(const uint16_t pIdx,
+    const uint8_t pSubIdx,
+    int64_t * const pOut)
+{
+    if(NULL == pOut) {
+        eprintf("[ERROR] OSCO <OSCOODGetI64> Output ptr is NULL !\n");
+        return OSCO_ERROR_ARG;
+    }
+
+    void *lObjectPtr = NULL;
+
+    /* Get the pointer to the objects data */
+    oscoErrorCode_t lErrorCode = getObjPtr(pIdx, pSubIdx, &lObjectPtr);
+    switch(lErrorCode) {
+        case OSCO_ERROR_NONE:
+            /* We found the object, and we can read it */
+            if(NULL == lObjectPtr) {
+                /* We shouldn't be in this case, something went wrong */
+                eprintf("[ERROR] OSCO <OSCOODGetI64> 0x%04X:0x%02X : getObjPtr returned a NULL pointer !\n", pIdx, pSubIdx);
+                return OSCO_ERROR_OD;
+            }
+            *pOut = *(int64_t *)lObjectPtr;
+            return OSCO_ERROR_NONE;
+        default:
+            return lErrorCode;
+    }
 
     return OSCO_ERROR_OD_NOT_FOUND;
 }
